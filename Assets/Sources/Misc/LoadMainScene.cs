b3e9c97ab1e5dadeafea942dffe00d600931ed20
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-09-12 19:02:04 +0200

    feat : added entry point scene that loads main scene to add to build list since all scenes marked as addressables cannot be added to the scene list

---
+++ Assets/Sources/Misc/LoadMainScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMainScene : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return SceneLoadSystem.LoadSceneAsyncAdditive("Assets/Scenes/Main", new SceneData<MainContext>());
+
+        //load main scene and immediately remove this useless scene that is just here because an addressable scene cannot be added to the unity scene list :upside_down_face
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+}
