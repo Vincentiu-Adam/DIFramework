d819743a1ae2feeb615f3b38e8932b28c3aa87ac
Vincentiu Dioanca-Adam <vincentiu.adam@gmail.com>
2024-09-13 10:11:31 +0200

    feat : added addressable script and assets

---
+++ Assets/Sources/Data/ArchetypeDataScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public enum ArchetypeType
+{
+    LIGHT,
+    MEDIUM,
+    HEAVY
+}
+
+[CreateAssetMenu(fileName = "unit_archetype_", menuName = "Scriptable Objects/ArchetypeDataScriptableObject")]
+public class ArchetypeDataScriptableObject : ScriptableObject
+{
+    public ArchetypeType Type;
+    public Vector2 Damage;
+    public Vector2 Health;
+    public Vector2 Armor;
+}
